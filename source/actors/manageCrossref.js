1ed065e7627505898908be9be6ab5dabbe32beaa
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2017-11-22 13:28:08 +0100

    Correct a bug causing bibtex to be requested several times

--- source/actors/manageCrossref.js
+++ source/actors/manageCrossref.js
@@ -131,57 +131,57 @@ export default function manageCrossref(store) {
         for (const [id, bibtexRequest] of state.bibtexRequests) {
             if (!bibtexRequest.fetching) {
                 store.dispatch(fetchBibtexFromDoi(id));
-            }
-            fetch(`https://dx.doi.org/${bibtexRequest.doi}`, {headers: new Headers({
-                'Accept': 'text/bibliography; style=bibtex',
-            })})
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error(response.statusText);
-                    }
-                    return response;
-                })
-                .then(response => response.text())
-                .then(rawText => {
-                    const text = rawText.trim();
-                    let bibtex = '';
-                    let nesting = 0;
-                    for (const character of text) {
-                        switch (character) {
-                            case '{':
-                                ++nesting;
-                                bibtex += character;
-                                break;
-                            case '}':
-                                --nesting;
-                                if (nesting === 0) {
-                                    bibtex += ',\n}';
-                                } else {
-                                    bibtex += character;
-                                }
-                                break;
-                            case ',':
-                                if (nesting < 2) {
-                                    bibtex += ',\n    ';
-                                } else {
-                                    bibtex += character;
-                                }
-                                break;
-                            default:
-                                bibtex += character;
-                                break;
+                fetch(`https://dx.doi.org/${bibtexRequest.doi}`, {headers: new Headers({
+                    'Accept': 'text/bibliography; style=bibtex',
+                })})
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error(response.statusText);
                         }
-                    }
-                    store.dispatch(resolveBibtexFromDoi(
-                        id,
-                        bibtexRequest.doi,
-                        bibtex + '\n'
-                    ));
-                })
-                .catch(error => {
-                    store.dispatch(rejectBibtexFromDoiConnection(id));
-                })
-            ;
+                        return response;
+                    })
+                    .then(response => response.text())
+                    .then(rawText => {
+                        const text = rawText.trim();
+                        let bibtex = '';
+                        let nesting = 0;
+                        for (const character of text) {
+                            switch (character) {
+                                case '{':
+                                    ++nesting;
+                                    bibtex += character;
+                                    break;
+                                case '}':
+                                    --nesting;
+                                    if (nesting === 0) {
+                                        bibtex += ',\n}';
+                                    } else {
+                                        bibtex += character;
+                                    }
+                                    break;
+                                case ',':
+                                    if (nesting < 2) {
+                                        bibtex += ',\n    ';
+                                    } else {
+                                        bibtex += character;
+                                    }
+                                    break;
+                                default:
+                                    bibtex += character;
+                                    break;
+                            }
+                        }
+                        store.dispatch(resolveBibtexFromDoi(
+                            id,
+                            bibtexRequest.doi,
+                            bibtex + '\n'
+                        ));
+                    })
+                    .catch(error => {
+                        store.dispatch(rejectBibtexFromDoiConnection(id));
+                    })
+                ;
+            }
         }
         for (const [id, doiRequest] of state.doiRequests) {
             if (!doiRequest.fetching) {
